24bd245cbc1482ece0b11d6b70df10b307b2237e
gardneca195 <gardneca195@gmail.com>
2017-01-20 21:23:51 -0500

    Update DataReader.java

--- DataReader.java
+++ DataReader.java
@@ -1,1 +1,24 @@
+public class DataReader{
+    private Scanner sc = new Scanner(new File("User.data");
 
+    public static boolean doesUserExist(String userName){
+        while(sc.hasNextLine()){
+            String data = sc.nextLine();
+            if(data.contains(userName)
+               return true;
+        }
+        return false;
+    }
+               
+    public static String validateID(String userName, String Password){
+       while(sc.hasNextLine()){
+           String data = sc.nextLine();
+           if(data.contiains(userName){
+               if(data.contains(password){
+                   return true;
+               }
+           }
+           return false;
+    }
+
+}
