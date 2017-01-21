7626810bda8db66132e8154bd17e62f1315aabc0
gardneca195 <gardneca195@gmail.com>
2017-01-20 20:59:04 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,24 @@
+public class User{
+  private String userName;
+  private String password;
+  private int height;
+  private int weight;
+  
+  public void User(String userName, String password, int height, int weight){
+    this.userName = userName;
+    this.password = password;
+    this.height = height;
+    this.weight = weight;
+  }
 
+  public void setWeight(int weight){
+    this.weight = weight;
+  }
+  
+  public void setHeight(int height){
+    this.height = height;
+  }
+  public void changePassword(String Password){
+    this.password = password;
+  }
+}
